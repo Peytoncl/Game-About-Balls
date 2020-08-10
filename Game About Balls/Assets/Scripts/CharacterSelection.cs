6b2aa5ccd43d0a7729334d98d4ceafc4354f3aa0
Peytoncl <peytonthegreat0@gmail.com>
2020-08-10 11:36:43 -0400

    Select Sound + Bootleg Select Script That Isnt Finished

--- Game About Balls/Assets/Scripts/CharacterSelection.cs
+++ Game About Balls/Assets/Scripts/CharacterSelection.cs
@@ -13,6 +13,12 @@ public class CharacterSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkInput();
+        checkDone();
+    }
+
+    void checkInput()
+    {
         if(Input.GetKeyDown(KeyCode.LeftArrow) && this.transform.position.x != -6.12f) 
         {
             this.transform.position = new Vector3(this.transform.position.x - 3.06f, this.transform.position.y);
@@ -20,6 +26,15 @@ public class CharacterSelection : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.RightArrow) && this.transform.position.x != 6.12f)
         {
             this.transform.position = new Vector3(this.transform.position.x + 3.06f, this.transform.position.y);
+        }
+    }
+
+    void checkDone()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+             RaycastHit2D hit = Physics2D.Raycast(this.transform.position, -Vector2.up);
+             Debug.Log(hit.transform.name);
         }
     }
 }
