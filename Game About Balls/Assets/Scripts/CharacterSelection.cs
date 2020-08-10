6a9a1e72d469a8f3b8c91f06a7c9d55e138e2aba
Peytoncl <peytonthegreat0@gmail.com>
2020-08-10 00:49:40 -0400

    Initialization

---
+++ Game About Balls/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && this.transform.position.x != -6.12f) 
+        {
+            this.transform.position = new Vector3(this.transform.position.x - 3.06f, this.transform.position.y);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow) && this.transform.position.x != 6.12f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + 3.06f, this.transform.position.y);
+        }
+    }
+}
